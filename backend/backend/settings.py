3570a5f8601e6919ecea400eb86d7a4a8d677542
Yash Hivarkar <115730629+Yashh2002@users.noreply.github.com>
2025-12-15 23:50:49 +0530

    Add allowed host for production environment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-!o2g^q=genm=c3_^o55$#s75ut&$qecugbiork(hr6052#_qdb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'job-application-tracker-gjtm.onrender.com',
+]
 
 
 # Application definition
