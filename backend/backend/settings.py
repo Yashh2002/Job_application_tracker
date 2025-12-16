c4480302e629ce759f4caee3cbd5b42eea1162f3
Yashh2002 <115730629+Yashh2002@users.noreply.github.com>
2025-12-16 09:22:35 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!o2g^q=genm=c3_^o55$#s75ut&$qecugbiork(hr6052#_qdb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'job-application-tracker-1-deic.onrender.com',
+    'job-application-tracker-gjtm.onrender.com',
+]
 
 
 # Application definition
