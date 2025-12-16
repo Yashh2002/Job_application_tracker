df87182b165a4d12dbd4fd4a88a7a1c6a8bf42bc
Yashh2002 <115730629+Yashh2002@users.noreply.github.com>
2025-12-16 13:37:15 +0530

    Fix production SECRET_KEY and DEBUG=False

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!o2g^q=genm=c3_^o55$#s75ut&$qecugbiork(hr6052#_qdb'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 
 
 ALLOWED_HOSTS = os.environ.get(
