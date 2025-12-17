50e3edb95d83317ddb7c3ec62e451995cf964a03
Yashh2002 <115730629+Yashh2002@users.noreply.github.com>
2025-12-17 16:06:34 +0530

    Debug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-dev-key-only-for-debug")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = [
